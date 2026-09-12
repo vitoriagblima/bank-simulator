5b4453da1305332b0aeb0857a7ef0ed8aa32a3b5
Vitória Lima <165972453+vitoriagblima@users.noreply.github.com>
2026-09-12 17:59:24 +0000

    feat(util): adiciona MonetarioConfig para padronizar scale e RoundingMode (#31)

---
+++ src/model/util/MonetarioConfig.java
@@ -0,0 +1,12 @@
+package model.util;
+
+import java.math.RoundingMode;
+
+public final class MonetarioConfig {
+
+    public static final int SCALE = 2;
+    public static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_EVEN;
+
+    private MonetarioConfig() {
+    }
+}
