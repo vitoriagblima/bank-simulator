4af2eb4b80f31da5303cd8d957f505065d67477b
Vitória Lima <165972453+vitoriagblima@users.noreply.github.com>
2026-09-12 19:46:42 +0000

    feat: adiciona SCALE_TAXA em MonetarioConfig (#39)

--- src/model/util/MonetarioConfig.java
+++ src/model/util/MonetarioConfig.java
@@ -5,6 +5,7 @@ import java.math.RoundingMode;
 public final class MonetarioConfig {
 
     public static final int SCALE = 2;
+    public static final int SCALE_TAXA = 4;
     public static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_EVEN;
 
     private MonetarioConfig() {
